f7f8d3138280806b9ae4e0dce212e1279e75f97c
jaideeptrades <jaideeps098@gmail.com>
2026-05-05 00:29:11 +1000

    Update index.js

--- index.js
+++ index.js
@@ -181,4 +181,5 @@ res.status(500).json({ error: err.message });
 
 app.get(”/”, (req, res) => res.send(“Apex Alert Server Running ✅”));
 
-app.listen(3000, () => console.log(“🚀 Server running on port 3000”));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
